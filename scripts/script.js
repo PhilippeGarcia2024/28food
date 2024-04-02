ef8fd78fafa035e4e7c837db0ab31c63628978e8
PhilippeGarcia2024 <bart10_lipe@hotmail.com>
2024-04-02 11:49:49 -0300

    ATUALIZACAO

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,16 @@ function showSlides() {
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+// Adiciona um evento de clique ao link do vídeo
+document.getElementById("video-link").addEventListener("click", function(event) {
+  event.preventDefault(); // Previne o comportamento padrão do link
+  
+  // Alterna a visibilidade do contêiner do vídeo
+  var videoContent = document.getElementById("video-content");
+  if (videoContent.style.display === "none") {
+      videoContent.style.display = "block";
+  } else {
+      videoContent.style.display = "none";
+  }
+});
